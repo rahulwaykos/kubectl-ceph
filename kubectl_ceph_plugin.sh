1e0937598bd86296cd576aaff6c02fa548770b48
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-12-29 15:20:04 +0530

    Update kubectl_ceph_plugin.sh

--- kubectl_ceph_plugin.sh
+++ kubectl_ceph_plugin.sh
@@ -3,14 +3,17 @@
 YUM=$(which yum)
 APT=$(which apt-get)
 
-if [ ! -z "$YUM" ]; then
+if [[ ! -z "$YUM" ]]; then
     yum install git -y > /dev/null
-    git clone https://github.com/rahulwaykos/kubectl-ceph ~/.ceph-plugin /dev/null
-    chmod -R ~./ceph-plugin
+    git clone https://github.com/rahulwaykos/kubectl-ceph ~/.ceph-plugin > /dev/null
+    rm ~/.ceph-plugin/kubectl_ceph_plugin.sh
+    chmod -R +x ~./ceph-plugin/
     mv ~./ceph-plugin/ /usr/local/bin > /dev/null
- elif [ ! -z "$APT" ]; then
+    
+ elif [[ ! -z "$APT" ]]; then
     apt-get install git -y > /dev/null
     git clone https://github.com/rahulwaykos/kubectl-ceph ~/.ceph-plugin > /dev/null
-    chmod -R ~./ceph-plugin
-    mv ~./ceph-plugin/ /usr/local/bin /dev/null
+    chmod -R +x ~./ceph-plugin
+    rm ~/.ceph-plugin/kubectl_ceph_plugin.sh
+    mv ~./ceph-plugin/ /usr/local/bin > /dev/null
 fi
